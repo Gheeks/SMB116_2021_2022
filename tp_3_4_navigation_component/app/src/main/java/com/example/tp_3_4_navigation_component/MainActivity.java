4b1c67c89fff11c4db818b66e1d76e4b414fbdbe
Jordan <hiertzjordan@gmail.com>
2022-01-11 17:00:52 +0100

    TP_3_4_Navigation_Component

---
+++ tp_3_4_navigation_component/app/src/main/java/com/example/tp_3_4_navigation_component/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tp_3_4_navigation_component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
